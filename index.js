465f606592f0bf036d29fe26e8499ee2bd5f722d
alexross2502 <93192857+alexross2502@users.noreply.github.com>
2023-11-27 15:32:27 +0200

    5555

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const PORT = process.env.PORT || 3306;
 const cors = require("cors");
 const bot = require("./bot.js");
-
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
 const mongoose = require("mongoose");
 const router = require("./routes");
 const dotenv = require("dotenv");
@@ -35,10 +38,54 @@ app.get("/", (req, res) => {
 const start = async () => {
   try {
     app.listen(PORT, () => console.log("start", PORT));
-    bot();
+    check();
   } catch (e) {
     console.log(e);
   }
 };
+
+async function check() {
+  try {
+    console.log(process.env.BOT_TOKEN);
+    await client.login(process.env.BOT_TOKEN);
+
+    async function doSomething() {
+      const serverID = "1177898865773531156";
+      const server = await client.guilds.cache.get(serverID);
+      if (server) {
+        server.channels
+          .fetch()
+          .then(async (channels) => {
+            const targetChannel = await channels.find(
+              (channel) => channel.name === "test"
+            );
+            if (targetChannel) {
+              await targetChannel.send(
+                "Привет! Я бот и я только что присоединился к серверу!"
+              );
+            } else {
+              console.error("Не удалось найти текстовый канал.");
+            }
+          })
+          .catch((error) => {
+            console.error("Ошибка при получении каналов сервера:", error);
+          });
+      } else {
+        console.error(`Сервер с ID ${serverID} не найден.`);
+      }
+    }
+    function scheduleTask(hour, minute, callback) {
+      // ... ваша функция
+    }
+    // Пример использования:
+    doSomething();
+    scheduleTask(3, 20, () => {
+      doSomething();
+    });
+    scheduleTask(3, 21, () => {
+      doSomething();
+    });
+  } catch (e) {}
+}
 
 start();
