eaf835b6f55cb2a06903447197b9f5d6d247a00f
alexross2502 <93192857+alexross2502@users.noreply.github.com>
2023-11-29 15:56:10 +0200

    test

--- controllers/tokenController.js
+++ controllers/tokenController.js
@@ -1,41 +1,40 @@
-const { Client, GatewayIntentBits, bold } = require("discord.js");
+const { Client, GatewayIntentBits } = require("discord.js");
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
-const express = require("express");
-const axios = require("axios");
 
-async function doSomething(firstText, secondText) {
-  await client.login(process.env.BOT_TOKEN);
-  const serverID = "1177898865773531156";
-  const server = await client.guilds.cache.get(serverID);
-  if (server) {
-    server.channels
-      .fetch()
-      .then(async (channels) => {
-        const targetChannel = await channels.find(
-          (channel) => channel.name === "test"
-        );
-        if (targetChannel) {
-          firstText = bold(firstText);
-          await targetChannel.send(`213421`);
-        } else {
-          console.error("Не удалось найти текстовый канал.");
-        }
-      })
-      .catch((error) => {
-        console.error("Ошибка при получении каналов сервера:", error);
-      });
-  } else {
-    console.error(`Сервер с ID ${serverID} не найден.`);
-  }
-}
-
-async function checker() {}
-
-async function check(req, res) {
+async function check() {
   try {
-    await doSomething("1", "1");
+    console.log(process.env.BOT_TOKEN);
+    await client.login(process.env.BOT_TOKEN);
+
+    async function doSomething() {
+      const serverID = "1177898865773531156";
+      const server = await client.guilds.cache.get(serverID);
+      if (server) {
+        server.channels
+          .fetch()
+          .then(async (channels) => {
+            const targetChannel = await channels.find(
+              (channel) => channel.name === "test"
+            );
+            if (targetChannel) {
+              await targetChannel.send(
+                "Привет! Я бот и я только что присоединился к серверу!"
+              );
+            } else {
+              console.error("Не удалось найти текстовый канал.");
+            }
+          })
+          .catch((error) => {
+            console.error("Ошибка при получении каналов сервера:", error);
+          });
+      } else {
+        console.error(`Сервер с ID ${serverID} не найден.`);
+      }
+    }
+
+    doSomething();
 
     res.status(200).json("ok").end();
   } catch (e) {
