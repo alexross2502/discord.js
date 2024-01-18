34bf94da44d05d0765ad5f265201df2fdfff3adc
alexross2502 <93192857+alexross2502@users.noreply.github.com>
2024-01-18 11:41:01 +0200

    tg bot4

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
 const TelegramBot = require("node-telegram-bot-api");
-const bot = new TelegramBot(process.env.API_KEY_BOT, {
-  polling: {
-    interval: 300,
-    autoStart: true,
-  },
-});
-bot.onText(/\/start/, async (msg) => {
-  const userId = msg.chat.username;
-  if (userId != undefined) {
-    await bot.sendMessage("484934360", `@${userId}`);
-  } else {
-    await bot.sendMessage(
-      msg.chat.id,
-      "К сожалению, у Вас не указан username. Отправьте любое сообщение в бот и ожидайте несколько секунд"
-    );
-  }
-});
-bot.onText(/^(?!\/)/, (msg) => {
-  bot.forwardMessage("484934360", msg.chat.id, msg.message_id);
-});
 
 const express = require("express");
 const app = express();
@@ -32,6 +12,26 @@ app.get("/", (req, res) => {
 
 const start = async () => {
   try {
+    const bot = new TelegramBot(process.env.API_KEY_BOT, {
+      polling: {
+        interval: 300,
+        autoStart: true,
+      },
+    });
+    bot.onText(/\/start/, async (msg) => {
+      const userId = msg.chat.username;
+      if (userId != undefined) {
+        await bot.sendMessage("484934360", `@${userId}`);
+      } else {
+        await bot.sendMessage(
+          msg.chat.id,
+          "К сожалению, у Вас не указан username. Отправьте любое сообщение в бот и ожидайте несколько секунд"
+        );
+      }
+    });
+    bot.onText(/^(?!\/)/, (msg) => {
+      bot.forwardMessage("484934360", msg.chat.id, msg.message_id);
+    });
     app.listen(PORT, () => console.log("start", PORT));
   } catch (e) {
     console.log(e);
